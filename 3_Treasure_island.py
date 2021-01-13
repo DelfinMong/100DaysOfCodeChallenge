e50f29554e06de92510cfd1c4f4b2ce129ecd161
delfin mongendu <delfdevs@gmail.com>
2021-01-12 23:58:01 -0500

    1

--- 3_Treasure_island.py
+++ 3_Treasure_island.py
@@ -1,3 +1,7 @@
 print("Welcome to Tresure Island.")
 print("Your mission is to find the treasure.")
-input('You\re at a crossroad, where do you want to go? Type "left" or "right"')
+choice1 = input('You\re at a crossroad, where do you want to go? Type "left" or "right"').lower()
+
+if choice1 == "left":
+    choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake.Type "wait" to wait for a boat').lower()
+    
